8194414ba45d8ed33660dc10c6cbefff2b92e04d
Shuhel15 <shuhelahmed3789@gmail.com>
2026-08-21 09:58:17 +0530

    Fix prisma migration table case

--- prisma/migrations/20260812071353_add_form_question_models/migration.sql
+++ prisma/migrations/20260812071353_add_form_question_models/migration.sql
@@ -1,37 +1,42 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `type` on the `question` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Enum(EnumId(0))`.
-  - Added the required column `updatedAt` to the `Question` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- DropForeignKey
-ALTER TABLE `form` DROP FOREIGN KEY `Form_userId_fkey`;
+ALTER TABLE `Form` DROP FOREIGN KEY `Form_userId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `question` DROP FOREIGN KEY `Question_formId_fkey`;
+ALTER TABLE `Question` DROP FOREIGN KEY `Question_formId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `submission` DROP FOREIGN KEY `Submission_formId_fkey`;
+ALTER TABLE `Submission` DROP FOREIGN KEY `Submission_formId_fkey`;
 
 -- DropIndex
-DROP INDEX `Form_userId_fkey` ON `form`;
+DROP INDEX `Form_userId_fkey` ON `Form`;
 
 -- AlterTable
-ALTER TABLE `question` ADD COLUMN `updatedAt` DATETIME(3) NOT NULL,
-    MODIFY `type` ENUM('SHORT_TEXT', 'LONG_TEXT', 'MULTIPLE_CHOICE', 'CHECKBOX', 'DROPDOWN', 'NUMBER', 'EMAIL', 'DATE') NOT NULL;
+ALTER TABLE `Question`
+ADD COLUMN `updatedAt` DATETIME(3) NOT NULL,
+MODIFY `type` ENUM('SHORT_TEXT', 'LONG_TEXT', 'MULTIPLE_CHOICE', 'CHECKBOX', 'DROPDOWN', 'NUMBER', 'EMAIL', 'DATE') NOT NULL;
 
 -- AddForeignKey
-ALTER TABLE `Form` ADD CONSTRAINT `Form_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Form`
+ADD CONSTRAINT `Form_userId_fkey`
+FOREIGN KEY (`userId`) REFERENCES `User`(`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Question` ADD CONSTRAINT `Question_formId_fkey` FOREIGN KEY (`formId`) REFERENCES `Form`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Question`
+ADD CONSTRAINT `Question_formId_fkey`
+FOREIGN KEY (`formId`) REFERENCES `Form`(`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Submission` ADD CONSTRAINT `Submission_formId_fkey` FOREIGN KEY (`formId`) REFERENCES `Form`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Submission`
+ADD CONSTRAINT `Submission_formId_fkey`
+FOREIGN KEY (`formId`) REFERENCES `Form`(`id`)
+ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- RenameIndex
-ALTER TABLE `question` RENAME INDEX `Question_formId_fkey` TO `Question_formId_idx`;
+ALTER TABLE `Question`
+RENAME INDEX `Question_formId_fkey` TO `Question_formId_idx`;
 
 -- RenameIndex
-ALTER TABLE `submission` RENAME INDEX `Submission_formId_fkey` TO `Submission_formId_idx`;
+ALTER TABLE `Submission`
+RENAME INDEX `Submission_formId_fkey` TO `Submission_formId_idx`;
